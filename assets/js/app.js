107566e890819614a1b640615dc4680ab4c00330
HansGit30 <homgit07@gmail.com>
2024-12-05 12:21:27 -0500

    animacion scrool

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,47 @@ menu.addEventListener("click", (event) => {
     ul.classList.toggle("none");
 });
 
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    // Aquí se permite que el elemento se active un poco antes, a los 150px del top
+    return rect.top <= window.innerHeight - 150 && rect.bottom >= 0;
+}
+
+
+const scroleadosDerecha = document.querySelectorAll('.scrollDerecha');
+const scroleadosIzquierda = document.querySelectorAll('.scrollIzquierda');
+const scroleadosArriba = document.querySelectorAll('.scrollArriba');
+const scroleadosAbajo = document.querySelectorAll('.scrollAbajo');
+
+
+
+function checkVisibility() {
+    scroleadosDerecha.forEach(section => {
+        if (isInViewport(section)) {
+            section.classList.add('visibleDerecha'); 
+        }
+    });
+    scroleadosIzquierda.forEach(section => {
+        if (isInViewport(section)) {
+            section.classList.add('visibleIzquierda'); 
+        }
+    });
+    scroleadosArriba.forEach(section => {
+        if (isInViewport(section)) {
+            section.classList.add('visibleArriba'); 
+        }
+    });
+    scroleadosAbajo.forEach(section => {
+        if (isInViewport(section)) {
+            section.classList.add('visibleAbajo'); 
+        }
+    });
+}
+
+window.addEventListener('scroll', checkVisibility);
+
+window.addEventListener('load', checkVisibility);
+
+
