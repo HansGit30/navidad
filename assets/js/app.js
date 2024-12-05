0436ecf8179e7cb36b30b937d32b3689913e4f8c
HansGit30 <homgit07@gmail.com>
2024-12-05 14:14:10 -0500

    render

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,23 @@
 const menu = document.querySelector(".hamburger");
 const ul =document.querySelector("#list");
-const li = document.querySelectorAll(".lista")
 const a = document.querySelectorAll(".enlace");
-
 menu.addEventListener("click", (event) => {
     if (event.target.tagName === "INPUT") return;
     ul.classList.toggle("none");
 });
-
 a.forEach((e)=>{
     e.addEventListener("click",()=>{
         ul.classList.toggle("none");
     })
 })
-
-
-
-
-
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
-    // Aquí se permite que el elemento se active un poco antes, a los 150px del top
     return rect.top <= window.innerHeight - 150 && rect.bottom >= 0;
 }
-
-
 const scroleadosDerecha = document.querySelectorAll('.scrollDerecha');
 const scroleadosIzquierda = document.querySelectorAll('.scrollIzquierda');
 const scroleadosArriba = document.querySelectorAll('.scrollArriba');
 const scroleadosAbajo = document.querySelectorAll('.scrollAbajo');
-
-
-
 function checkVisibility() {
     scroleadosDerecha.forEach(section => {
         if (isInViewport(section)) {
@@ -54,9 +40,7 @@ function checkVisibility() {
         }
     });
 }
-
 window.addEventListener('scroll', checkVisibility);
-
 window.addEventListener('load', checkVisibility);
 
 
